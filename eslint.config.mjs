4c425560a7f41c698315c0756c45d003d1fc68c7
RaulSAraujo <raul.araujo.2000@hotmail.com>
2024-06-26 17:37:25 -0300

    Feat: configuração do eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
-// @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
   // Your custom configs here
+  {
+    rules: {
+      'vue/valid-v-slot': 'off'
+    }
+  }
 )
