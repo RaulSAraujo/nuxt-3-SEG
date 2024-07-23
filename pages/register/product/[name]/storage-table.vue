9014334280ae85cd4617b18398968b9446b83d81
RaulSAraujo <raul.araujo.2000@hotmail.com>
2024-07-23 16:44:51 -0300

    ✨ feat: new page storage

--- pages/register/product/[name]/storage-table.vue
+++ pages/register/product/[name]/storage-table.vue
@@ -1,7 +1,123 @@
 <script setup lang="ts">
+import type { Product } from "~/interfaces/Product.js";
+import type { ERPWarehouses } from "~/interfaces/ErpWarehouses.js";
+const { params } = useRoute();
 
+const [warehousesRes, erpWarehousesRes] = await Promise.all([
+  $api<Product>(`product?name=${params.name}&warehouses=true&unique=true`),
+  $api<ERPWarehouses>(`erp-warehouses`),
+]);
+
+const PreferentialWarehouse =
+  warehousesRes.data.value?.rows[0]?.PreferentialWarehouse ?? null;
+const WarehousesAddresses = warehousesRes.data.value?.rows[0]?.WarehousesAddresses ?? [];
+const erpWarehouses = erpWarehousesRes.data.value?.rows ?? [];
+
+const headers = ref([
+  {
+    title: "ALMOXARIFADO",
+    key: "almox",
+  },
+  {
+    title: "LOCALIZAÇÃO",
+    key: "localizacao",
+  },
+  {
+    title: "ESTOQUE",
+    key: "estatual",
+  },
+  {
+    title: "ESTOQUE MIN.",
+    key: "estminimo",
+  },
+  {
+    title: "DATA COMPRA",
+    key: "dtcompra",
+  },
+  {
+    title: "DATA VENDA",
+    key: "dtvenda",
+  },
+]);
 </script>
 
 <template>
- <span>storage table</span>    
+  <v-container fluid>
+    <span class="text-h5 font-weight-bold">ARMAZENAGEM PREFERENCIAL</span>
+
+    <v-row v-if="PreferentialWarehouse" dense class="mt-3 mx-2">
+      <v-col>
+        <TextField
+          v-model="PreferentialWarehouse.almox"
+          label="CÓDIGO"
+          :disabled="true"
+        />
+      </v-col>
+      <v-col>
+        <Select
+          v-model="PreferentialWarehouse.almox"
+          label="ALMOXARIFADO"
+          :items="erpWarehouses"
+          item-title="descricao"
+          item-value="codigo"
+          :multiple="false"
+          disabled
+        />
+      </v-col>
+      <v-col>
+        <TextField
+          v-model="PreferentialWarehouse.estatual"
+          label="ESTOQUE"
+          :disabled="true"
+        />
+      </v-col>
+      <v-col>
+        <TextField
+          v-model="PreferentialWarehouse.localizacao"
+          label="LOCALIZAÇÃO"
+          :disabled="true"
+        />
+      </v-col>
+    </v-row>
+    <v-row v-else justify="center" class="py-6">
+      <span>Este produto não possui armazém Preferencial.</span>
+    </v-row>
+
+    <v-data-table
+      :headers="headers"
+      :items="WarehousesAddresses"
+      hide-default-footer
+      disable-filtering
+      disable-sort
+      disable-pagination
+    >
+      <template #top>
+        <v-toolbar>
+          <v-toolbar-title>LOCAIS DE ARMAZENAGEM</v-toolbar-title>
+        </v-toolbar>
+      </template>
+
+      <template #item.almox="{ item }">
+        <span v-for="(it, index) in erpWarehouses" :key="index">
+          <span v-if="item.almox == it.codigo">{{ it.descricao }}</span>
+        </span>
+      </template>
+
+      <template #item.estatual="{ item }">
+        <span>{{ parseInt(`${item.estatual ?? 0}`) }}</span>
+      </template>
+
+      <template #item.estminimo="{ item }">
+        <span>{{ parseInt(`${item.estminimo ?? 0}`) }}</span>
+      </template>
+
+      <template #item.dtcompra="{ item }">
+        <DateString :date="item.dtcompra" />
+      </template>
+
+      <template #item.dtvenda="{ item }">
+        <DateString :date="item.dtvenda" />
+      </template>
+    </v-data-table>
+  </v-container>
 </template>
