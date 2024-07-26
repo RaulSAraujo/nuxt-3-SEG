e603d5ebd5a9a3a69abcba34179df4f98d7e10b9
RaulSAraujo <raul.araujo.2000@hotmail.com>
2024-07-26 11:37:02 -0300

    ✨ feat: new page supplier

---
+++ components/supplier/Creation.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import type { Row } from "~/interfaces/Supplier.js";
+
+const emit = defineEmits(["close"]);
+
+interface Body {
+  name: string;
+  active: boolean;
+  lead_time: string | number;
+  virtual_quantity: string | number;
+}
+
+const supplier = reactive<Body>({
+  name: "",
+  active: true,
+  lead_time: "",
+  virtual_quantity: "",
+});
+
+const tableStore = useTableStore();
+const { items } = storeToRefs(tableStore);
+
+const loading = useLoadingIndicator();
+
+const create = async () => {
+  loading.start();
+
+  try {
+    const res = await useNuxtApp().$customFetch<Row>("supplier", {
+      method: "POST",
+      body: {
+        name: supplier.name.toUpperCase(),
+        active: supplier.active,
+        lead_time: supplier.lead_time,
+        virtual_quantity: supplier.virtual_quantity,
+      },
+    });
+
+    $toast().success("Novo fornecedor criado.");
+
+    close();
+
+    items.value.splice(0, 0, res);
+  } catch (error) {
+    const err = error as { statusText: string; message: string };
+
+    $toast().error(`${err.statusText ?? err.message}`);
+  }
+
+  loading.finish();
+};
+
+const close = () => {
+  supplier.name = "";
+  supplier.lead_time = "";
+  supplier.virtual_quantity = "";
+
+  emit("close");
+};
+</script>
+
+<template>
+  <v-dialog width="260px" persistent>
+    <v-card rounded="xl" title="NOVO FORNECEDOR">
+      <template #text>
+        <TextField v-model="supplier.name" label="NOME DO FORNECEDOR" />
+        <TextField v-model="supplier.lead_time" label="LEAD TIME" />
+        <TextField v-model="supplier.virtual_quantity" label="QUANTIDADE VIRTUAL" />
+      </template>
+
+      <template #actions>
+        <v-btn color="error" text="FECHAR" @click="close" />
+        <v-btn color="primary" text="SALVAR" @click="create" />
+      </template>
+    </v-card>
+  </v-dialog>
+</template>
--- pages/register/supplier/index.vue
+++ pages/register/supplier/index.vue
@@ -1,3 +1,80 @@
+<script setup lang="ts">
+import type { RouteLocationNormalizedLoaded } from "#vue-router";
+
+useHead({
+  titleTemplate: `Fornecedores - %s`,
+});
+
+const { findModelName } = useModelStore();
+const { name }: RouteLocationNormalizedLoaded = useRoute();
+findModelName(name?.toString() ?? "");
+
+const tableStore = useTableStore();
+const { url } = storeToRefs(tableStore);
+url.value = "supplier";
+
+const filterStore = useFilterStore();
+const { activeCreateButton } = storeToRefs(filterStore);
+activeCreateButton.value = true;
+
+const dialog = ref<boolean>(false);
+</script>
+
 <template>
-    <span>supplier</span>
+  <v-main>
+    <v-sheet class="mt-5" rounded="t-xl" elevation="5">
+      <Filter @create="dialog = true" />
+
+      <Table
+        title="FORNECEDORES"
+        :disabled-menu="false"
+        :show-select="true"
+        :multi-sort="true"
+      >
+        <template #item.action="{ item }">
+          <Delete :id="item.id" />
+        </template>
+
+        <template #item.active="{ item }">
+          <CheckIcon
+            :id="item.id"
+            :value="item.active ?? false"
+            attr="active"
+            :enable-alert="true"
+            @update-value="item.active = $event"
+          />
+        </template>
+
+        <template #item.name="{ item }">
+          <EditDialogString
+            :id="item.id"
+            :text="item.name ?? ''"
+            attr="name"
+            :vw="8"
+            @update-text="item.name = $event"
+          />
+        </template>
+
+        <template #item.virtual_quantity="{ item }">
+          <EditDialogNumber
+            :id="item.id"
+            :value="item.virtual_quantity ?? 0"
+            attr="virtual_quantity"
+            @update-text="item.virtual_quantity = $event"
+          />
+        </template>
+
+        <template #item.lead_time="{ item }">
+          <EditDialogNumber
+            :id="item.id"
+            :value="item.lead_time ?? 0"
+            attr="lead_time"
+            @update-text="item.lead_time = $event"
+          />
+        </template>
+      </Table>
+    </v-sheet>
+
+    <SupplierCreation v-model="dialog" @close="dialog = false" />
+  </v-main>
 </template>
