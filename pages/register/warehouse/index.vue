1223c49cbe4596594d288c98b7c8e386a2bb440f
RaulSAraujo <raul.araujo.2000@hotmail.com>
2024-07-29 08:47:11 -0300

    ✨ feat: new page warehouse

--- pages/register/warehouse/index.vue
+++ pages/register/warehouse/index.vue
@@ -1,3 +1,34 @@
+<script setup lang="ts">
+import type { RouteLocationNormalizedLoaded } from "#vue-router";
+
+useHead({
+  titleTemplate: `Almoxarifados - %s`,
+});
+
+const { findModelName } = useModelStore();
+const { name }: RouteLocationNormalizedLoaded = useRoute();
+findModelName(name?.toString() ?? "");
+
+const tableStore = useTableStore();
+const { url } = storeToRefs(tableStore);
+url.value = "erp-warehouses";
+
+const filterStore = useFilterStore();
+const { activeCreateButton } = storeToRefs(filterStore);
+activeCreateButton.value = false;
+</script>
+
 <template>
-    <span>warehouse</span>
+  <v-main>
+    <v-sheet class="mt-5" rounded="t-xl" elevation="5">
+      <Filter />
+
+      <Table
+        title="ALMOXARIFADOS"
+        :disabled-menu="false"
+        :show-select="true"
+        :multi-sort="true"
+      />
+    </v-sheet>
+  </v-main>
 </template>
