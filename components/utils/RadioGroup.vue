42d4bbbc758cc7a43cbc09a98ca2f54080a1810b
RaulSAraujo <raul.araujo.2000@hotmail.com>
2024-07-31 11:29:03 -0300

    ✨ feat: new component

---
+++ components/utils/RadioGroup.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  label?: string;
+  inline?: boolean;
+  hideDetails?: boolean | "auto" | undefined;
+  color?: string;
+  disabled?: boolean;
+  appendIcon?: string;
+  prependIcon?: string;
+}>();
+</script>
+
+<template>
+  <v-radio-group
+    :label="label"
+    :inline="inline || true"
+    :hide-details="hideDetails"
+    :color="color || 'blue'"
+    :disabled="disabled"
+    :append-icon="appendIcon"
+    :prepend-icon="prependIcon"
+  >
+    <slot />
+  </v-radio-group>
+</template>
