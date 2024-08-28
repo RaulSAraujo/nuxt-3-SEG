bf58cea24a583291520e80ba9964155b7ef2306e
RaulSAraujo <raul.araujo.2000@hotmail.com>
2024-08-28 16:04:28 -0300

    ✨ feat: new token

--- plugins/maska.ts
+++ plugins/maska.ts
@@ -5,6 +5,8 @@ export default defineNuxtPlugin((nuxtApp) => {
     tokens['S'] = { pattern: /[a-zA-Z]/ }
     tokens['A'] = { pattern: /[a-zA-Z]/, transform: (char) => char.toUpperCase() }
     tokens['a'] = { pattern: /[a-zA-Z]/, transform: (char) => char.toLowerCase() }
+    tokens['+'] = { pattern: /[0-9]/, multiple: true }
+    tokens['X'] = { pattern: /[A-Z0-9]/, transform: (char) => char.toUpperCase(), repeated: true }
 
     nuxtApp.vueApp.directive('maska', vMaska)
 })
