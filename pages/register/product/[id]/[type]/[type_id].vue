55eaf579e08870419130c5f64ea122fbec58873e
RaulSAraujo <raul.araujo.2000@hotmail.com>
2024-08-28 16:09:46 -0300

    💄 style: remove color

--- pages/register/product/[id]/[type]/[type_id].vue
+++ pages/register/product/[id]/[type]/[type_id].vue
@@ -3,9 +3,7 @@
 <template>
   <v-container fluid>
     <div class="d-flex flex-row justify-space-between mb-5">
-      <p class="text-h5 font-weight-bold text-primary">
-        FAMILIA ATUAL: {{ $route.params.type_id }}
-      </p>
+      <p class="text-h5 font-weight-bold">FAMILIA ATUAL: {{ $route.params.type_id }}</p>
 
       <ProductTypeGroupButtons />
     </div>
