cc4ffd88358d9be82b6ff914a82347d8c4a7f11f
RaulSAraujo <raul.araujo.2000@hotmail.com>
2024-09-09 11:11:06 -0300

    ✨ feat: new component

---
+++ components/utils/edit_dialog/Currency.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+const props = defineProps<{
+  id: number;
+  attr: string;
+  value: string;
+  vw: number;
+}>();
+
+const emit = defineEmits(["updateText"]);
+
+const { url } = useTableStore();
+
+const menu = ref<boolean>(false);
+
+const comp = computed({
+  get: () => props.value,
+  set: async (value) => {
+    const res = await update(value);
+    if (!res.success) return $toast().error(res.message);
+
+    menu.value = false;
+
+    emit("updateText", value);
+  },
+});
+
+const update = (value: string) => {
+  interface Response {
+    message: string;
+    result: [];
+    success: boolean;
+  }
+
+  return useNuxtApp().$customFetch<Response>(url, {
+    method: "PUT",
+    body: {
+      id: props.id,
+      [props.attr]: value,
+    },
+  });
+};
+</script>
+
+<template>
+  <v-menu
+    v-model="menu"
+    location="end"
+    transition="slide-x-transition"
+    offset="0 20"
+    :close-on-content-click="false"
+  >
+    <template #activator="{ props: menuProp }">
+      <ToLocaleString v-bind="menuProp" style="cursor: pointer" :value="value" />
+    </template>
+
+    <v-confirm-edit v-model="comp">
+      <template #default="{ model: proxyModel, actions }">
+        <v-card class="mx-auto" width="250px">
+          <template #text>
+            <CurrencyField v-model="proxyModel.value" />
+          </template>
+
+          <template #actions>
+            <v-spacer />
+
+            <component :is="actions" />
+          </template>
+        </v-card>
+      </template>
+    </v-confirm-edit>
+  </v-menu>
+</template>
