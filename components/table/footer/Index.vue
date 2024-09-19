97e58e75db14b15026d38a4f485f408af292f69a
RaulSAraujo <raul.araujo.2000@hotmail.com>
2024-09-19 09:24:43 -0300

    ✨ feat: add total page

--- components/table/footer/Index.vue
+++ components/table/footer/Index.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 const store = useTableStore();
-const { page, pageCount, itemsPerPage, itemsPerPageoptions } = storeToRefs(store);
+const { page, pageCount, itemsPerPage, itemsPerPageoptions, totalItems } = storeToRefs(
+  store
+);
 
 const { y } = useWindowScroll();
 
@@ -15,23 +17,27 @@ watch(itemsPerPage, () => {
 
 <template>
   <v-lazy :options="{ threshold: 0.5 }" transition="fade-transition">
-    <div class="d-flex flex-rows justify-end align-center">
-      <span class="mr-2 text-caption">Itens por página: </span>
+    <div class="d-flex flex-rows justify-space-between align-center">
+      <span class="ml-3 text-caption">TOTAL: {{ totalItems }}</span>
 
-      <v-select
-        v-model="itemsPerPage"
-        density="compact"
-        color="primary"
-        variant="outlined"
-        :items="itemsPerPageoptions"
-        item-title="title"
-        item-value="value"
-        max-width="90px"
-        hide-details
-        class="align-self-center"
-      />
+      <div class="d-flex flex-rows justify-end align-center">
+        <span class="mr-2 text-caption">Itens por página: </span>
 
-      <v-pagination v-model="page" :length="pageCount" :total-visible="7" />
+        <v-select
+          v-model="itemsPerPage"
+          density="compact"
+          color="primary"
+          variant="outlined"
+          :items="itemsPerPageoptions"
+          item-title="title"
+          item-value="value"
+          max-width="90px"
+          hide-details
+          class="align-self-center"
+        />
+
+        <v-pagination v-model="page" :length="pageCount" :total-visible="7" />
+      </div>
     </div>
   </v-lazy>
 </template>
