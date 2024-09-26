e741b53eb9b894dc279e4640bcb444770af3a12e
RaulSAraujo <raul.araujo.2000@hotmail.com>
2024-09-26 08:50:51 -0300

    feat: add iframe automation

--- pages/automation/puppeteer/index.vue
+++ pages/automation/puppeteer/index.vue
@@ -1,3 +1,9 @@
 <template>
-  <span>puppeteer</span>
+  <ClientOnly>
+    <iframe
+      src="http://localhost:3000/automation/puppeteer"
+      frameBorder="0"
+      style="height: 90vh; width: 100vw"
+    />
+  </ClientOnly>
 </template>
