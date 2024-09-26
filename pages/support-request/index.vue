67cea70450a6ab2d73e19154259797e45f38a432
RaulSAraujo <raul.araujo.2000@hotmail.com>
2024-09-26 07:41:11 -0300

    🐛 fix: template root

--- pages/support-request/index.vue
+++ pages/support-request/index.vue
@@ -5,38 +5,42 @@ useHead({
 </script>
 
 <template>
-  <Filter hide-excel>
-    <template #button-create>
-      <v-btn
-        class="mr-2"
-        color="primary"
-        text="CRIAR"
-        :to="{
-          name: 'support-request-creation',
-        }"
-      />
-    </template>
-  </Filter>
+  <div class="mt-5">
+    <span class="ml-5 text-h6 text-primary font-weight-black">CHAMADOS</span>
 
-  <Table show-expand>
-    <template #item.action="{ item }">
-      <Delete :id="item.id" />
-    </template>
+    <Filter hide-excel>
+      <template #button-create>
+        <v-btn
+          class="mr-2"
+          color="primary"
+          text="CRIAR"
+          :to="{
+            name: 'support-request-creation',
+          }"
+        />
+      </template>
+    </Filter>
 
-    <template #item.user_id="{ item }">
-      <span>{{ item.User.name.toUpperCase() }}</span>
-    </template>
+    <Table show-expand>
+      <template #item.action="{ item }">
+        <Delete :id="item.id" />
+      </template>
 
-    <template #item.priority="{ item }">
-      <SupportRequestTemplatePriority :priority="item.priority" />
-    </template>
+      <template #item.user_id="{ item }">
+        <span>{{ item.User.name.toUpperCase() }}</span>
+      </template>
 
-    <template #expanded-row="{ columns, item }">
-      <tr>
-        <td :colspan="columns.length">
-          <Tiptap :content="item.description" :editable="false" />
-        </td>
-      </tr>
-    </template>
-  </Table>
+      <template #item.priority="{ item }">
+        <SupportRequestTemplatePriority :priority="item.priority" />
+      </template>
+
+      <template #expanded-row="{ columns, item }">
+        <tr>
+          <td :colspan="columns.length">
+            <Tiptap :content="item.description" :editable="false" />
+          </td>
+        </tr>
+      </template>
+    </Table>
+  </div>
 </template>
