3bc3ce0d7264ad6716c90382331674bea255d5a5
RaulSAraujo <raul.araujo.2000@hotmail.com>
2024-09-30 13:37:12 -0300

    ✨ feat: token only numbers

--- components/product/details/Package.vue
+++ components/product/details/Package.vue
@@ -24,6 +24,7 @@ const { status } = $api<Row[]>("package", {
     <v-col cols="12" sm="6" md="3" lg="3" xl="3">
       <TextField
         v-model="product!.weight"
+        v-maska="'N'"
         label="Peso liquido (g)"
         :hide-details="true"
       />
--- plugins/maska.ts
+++ plugins/maska.ts
@@ -5,8 +5,9 @@ export default defineNuxtPlugin((nuxtApp) => {
     tokens['S'] = { pattern: /[a-zA-Z]/ }
     tokens['A'] = { pattern: /[a-zA-Z]/, transform: (char) => char.toUpperCase() }
     tokens['a'] = { pattern: /[a-zA-Z]/, transform: (char) => char.toLowerCase() }
-    tokens['+'] = { pattern: /^[0-9\W]+$/, multiple: true }
-    tokens['X'] = { pattern: /^[a-zA-Z0-9\W]+$/, transform: (char) => char.toUpperCase(), repeated: true }
+    tokens['N'] = { pattern: /^[0-9]+$/, multiple: true } // Aceita numeros
+    tokens['+'] = { pattern: /^[0-9\W]+$/, multiple: true } // Aceita numeros e caracter especial
+    tokens['X'] = { pattern: /^[a-zA-Z0-9\W]+$/, transform: (char) => char.toUpperCase(), repeated: true } // Aceita numeros, letras e caracter especial
 
     nuxtApp.vueApp.directive('maska', vMaska)
 })
