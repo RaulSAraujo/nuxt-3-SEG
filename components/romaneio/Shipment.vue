bba058e5b8905509dbab15518629f460617097f1
RaulSAraujo <raul.araujo.2000@hotmail.com>
2024-09-30 08:23:07 -0300

    ✨ feat: Sort the carriers

--- components/romaneio/Shipment.vue
+++ components/romaneio/Shipment.vue
@@ -3,9 +3,10 @@ import type { ShipmentOrders } from "~/interfaces/ShipmentOrders.js";
 
 const { data } = await $api<ShipmentOrders>("shipment-orders");
 
-const uniqueCollectCompanies = [
-  ...new Set(data.value?.rows.map((e) => e.collect_company.toUpperCase().trim())),
-];
+const uniqueCollectCompanies = useSorted(
+  [...new Set(data.value?.rows.map((e) => e.collect_company.toUpperCase().trim()))],
+  (a, b) => a.localeCompare(b)
+);
 </script>
 
 <template>
