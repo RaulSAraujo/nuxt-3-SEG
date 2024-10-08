4d859887fbefb8e4dc7426113ad6704460df7192
RaulSAraujo <raul.araujo.2000@hotmail.com>
2024-10-08 20:23:34 -0300

    🐛 fix: date is null

--- composables/useDateConversion.ts
+++ composables/useDateConversion.ts
@@ -2,22 +2,32 @@ export default function () {
     const dayjs = useDayjs()
 
     const brDate = (date: string) => {
+        if (!date) return ''
+
         return dayjs(date, "DD/MM/YYYY").isValid() ? date : dayjs(date, "YYYY-MM-DD").format("DD/MM/YYYY");
     }
 
     const brDateWithTime = (date: string) => {
+        if (!date) return ''
+
         return dayjs(date, "DD/MM/YYYY HH:mm:ss").isValid() ? date : dayjs(date, "YYYY-MM-DDTHH:mm:ss").format("DD/MM/YYYY HH:mm:ss");
     }
 
     const databaseDate = (date: string) => {
+        if (!date) return ''
+
         return dayjs(date, "YYYY-MM-DD").isValid() ? date : dayjs(date, "DD/MM/YYYY").format("YYYY-MM-DD");
     }
 
     const databaseDateWithTime = (date: string) => {
+        if (!date) return ''
+
         return dayjs(date, "YYYY-MM-DDTHH:mm:ss").isValid() ? date : dayjs(date, "DD/MM/YYYY HH:mm:ss").format("YYYY-MM-DDTHH:mm:ss");
     }
 
     const isDate = (date: string) => {
+        if (!date) return ''
+
         return dayjs(date, "DD/MM/YYYY").isValid()
     }
 
